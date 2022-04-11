164233c4d7feba3e08404b0bd540bd868b8b9569
Khelm2 <98402879+Khelm2@users.noreply.github.com>
2022-04-11 16:05:03 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -23,7 +23,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = 'red';
       if (feature.properties.severity === 'Minor') alertColor = 'green' ;
-      return { color: alertColor } ,
+      return { color: alertColor }
     },
     // Add a popup on each feature showing the NWS alert headline
     onEachFeature: function(feature, layer) {
